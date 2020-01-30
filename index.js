2e382774ae6fbf8f377e3c305734caf7a0e6cc11
Brittany Javalera <brittjavalera@gmail.com>
2020-01-30 13:49:01 -0800

    Done.

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function map(sourceArray, callFunction) {
   }
 
 function reduce(sourceArray, cbFunction, startingValue){
-    
+        let start =  !!startingValue ? startingValue : sourceArray[0]
+        for (let i = 0; i < sourceArray.length; i++){
+           let result = cbFunction(sourceArray[i], start)
+        }
+       return result
 }
