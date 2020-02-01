896a30115b99f245d94d9c6fa7046b57bd246af8
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-01 19:48:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, fn){
+  let newArr = []
+  arr.forEach(el => { 
+    newArr.push(fn(el))
+  })
+  return newArr;
+}
+
+function reduce(arr, fn, startingPoint){
+  let updateStart = (!!startingPoint) ? startingPoint : arr[0];
+  
+  let i = (!!startingPoint) ? 0 : 1;
+  
+   for (; i < arr.length; i++){
+    updateStart = fn(arr[i], updateStart)
+  }
+  return updateStart;
+}
