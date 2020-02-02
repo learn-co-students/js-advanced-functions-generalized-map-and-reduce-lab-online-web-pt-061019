b5dfb52a3bdd9ad9b700adc71938f10bc38daca3
github username <github email address>
2020-02-02 14:21:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+const map = function(arr, fn){
+    let newArr = []
+    for(const el of arr) {
+        newArr.push(fn(el))
+    }
+    return newArr
+}
+
+const reduce = function(arr, fn, startingPoint) {
+    let newStartPt = !!startingPoint ? startingPoint : arr[0]
+    let index = !!startingPoint ? 0 : 1
+    for ( ; index < arr.length; index++ ) {
+        newStartPt = fn(arr[index], newStartPt)
+    }
+    return newStartPt
+}
