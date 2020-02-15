f6b99e95415eabd76c811603f6a34d0ece95bd38
tshuttles <tshuttlesworth@gmail.com>
2020-02-14 18:17:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, callbackFn) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+      newArray.push(callbackFn(array[i]))
+  }
+  return newArray
+};
+
+function reduce(array, callbackFn, value) {
+  let memo = (!!value) ? value : array[0]
+  let i = (!!value) ? 0 : 1
+  for (; i < array.length; i++) {
+      memo = callbackFn(array[i], memo)
+  }
+  return memo
+};
